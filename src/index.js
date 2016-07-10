86c43d7447c95a6f0bb76f3e332b6510efc9b02f
nir@500tech.com <nir@500tech.com>
2016-07-10 15:36:25 +0300

    bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import angular from 'angular';
 
+// define angular module
 angular.module('app', [])
 
-  .component('main', {
+  // create a root component
+  .component('root', {
       template: `
        <h1>Hello AngularJS!</h1>
       `
   });
+
+// bootstrap the root component
+angular.element(document).ready(function() {
+  angular.bootstrap(document, ['app']);
+});
